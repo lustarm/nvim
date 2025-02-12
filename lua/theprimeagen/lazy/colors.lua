de454b4289bb3d516cd91975758d0d5df11c8772
jeweler <valve.valve101@gmail.com>
2025-02-12 03:15:09 -0500

    indents, and colorscheme

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -1,13 +1,15 @@
 function ColorMyPencils(color)
-	color = color or "gruvbox"
+	color = color or "vscode"
 	vim.cmd.colorscheme(color)
+--    vim.o.background("dark")
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
--- return { "rose-pine/neovim", name = "rose-pine", config = function() ColorMyPencils() end}
+return { "Mofiqul/vscode.nvim", config = function() ColorMyPencils() end}
 
-return { "ellisonleao/gruvbox.nvim", priority = 1000 , config = function ()
-    ColorMyPencils()
-end, opts = ...}
+
+-- return { "ellisonleao/gruvbox.nvim", priority = 1000 , config = function ()
+--     ColorMyPencils()
+-- end, opts = ...}
